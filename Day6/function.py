6ef256b41c530517d501c69f7dd0e0386c36b522
vkzs6 <arthur6@edu.univali.br>
2025-10-30 22:13:30 -0300

    add new day, new task of function

---
+++ Day6/function.py
@@ -0,0 +1,14 @@
+#  - Projeto: Bootcamp - 100 Days of Code – The Complete Python 
+#  - Arquivo: function.py
+#  - Desenvolvedor: Arthur de Queiroz
+#  - Data: Outubro/2025
+#  - Descrição: Compreendendo funções em python, introdução
+
+print("Hello") #this is one function
+
+def my_first_function():
+    print("This isn't my first function")
+    print("Y/N")
+
+
+my_first_function()
